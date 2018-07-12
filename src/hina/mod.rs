2becdf832b359921d0c1c51ce4e864aa660a2ceb
annie <a@hinaria.com>
2018-07-12 03:18:37 -0700

    document unsafe safety.

--- src/hina/mod.rs
+++ src/hina/mod.rs
@@ -10,7 +10,6 @@ use {
 
 // convert a singular `T` into a single element slice of `T`.
 crate fn as_slice<T>(item: &T) -> &[T] {
-    unsafe {
-        std::slice::from_raw_parts(item, 1)
-    }
+    // safe: the memory layout of a singular `T` is always the same as an array of one `T`.
+    unsafe { std::slice::from_raw_parts(item, 1) }
 }
